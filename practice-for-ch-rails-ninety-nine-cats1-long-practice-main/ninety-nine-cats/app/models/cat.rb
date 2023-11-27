8c22474a17096cdc592ee9fa8371c0b3c3482aa3
Peter Nolan <peter.l.nolan@gmail.com>
2023-11-27 14:47:28 -0500

    Add validations

--- practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/app/models/cat.rb
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/app/models/cat.rb
@@ -1,3 +1,7 @@
 class Cat < ApplicationRecord
+
+
     validates :birth_date, :name, presence: true
+    validates :color, inclusion: { in: CAT_COLORS }
+    validates :sex, inclusion: { in: %w(M F) }
 end
