2fedef844929a7a54c58e860819175b49399c928
Jacob Tabor <jtabor214@gmail.com>
2023-11-27 15:08:20 -0500

    Completed custom validation; defined class Constant

--- practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/app/models/cat.rb
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/app/models/cat.rb
@@ -1,7 +1,19 @@
 class Cat < ApplicationRecord
 
+    CAT_COLORS = %w(grey black orange calico white brown blue-grey)
 
     validates :birth_date, :name, presence: true
     validates :color, inclusion: { in: CAT_COLORS }
     validates :sex, inclusion: { in: %w(M F) }
+    validate :birth_date_cannot_be_future 
+
+private
+
+    def birth_date_cannot_be_future 
+        if birth_date > Date.today 
+            errors.add(:birth_date, "cannot be in the future")
+        end
+    end
+
+
 end
